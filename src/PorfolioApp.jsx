2b8a7646b4f444a77600cdb10c0f083584100f1a
Jose Manuel Borras <josemanuel.borras@gmail.com>
2026-04-24 13:25:37 -0300

    React: 03. Componentes - 02. Creando el primero componente

---
+++ src/PorfolioApp.jsx
@@ -0,0 +1,11 @@
+import './porfolioApp.css'
+
+export const PorfolioApp = () => {
+  return (
+    <div>
+      <h1 className='rojo'>PorfolioApp</h1>
+      <h4>Subtitulo</h4>
+      <p>Hola Mundo</p>
+    </div>
+  )
+}
