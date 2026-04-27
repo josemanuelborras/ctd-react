3cc5e8c39648cccdbed28cbb362e58ad2be52bc2
Jose Manuel Borras <josemanuel.borras@gmail.com>
2026-04-27 10:01:32 -0300

    React: 03. Componentes - 03. Componente de clase

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PorfolioApp } from './PorfolioApp'
+import ClassComponent from './ClassComponent'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PorfolioApp />
+    <ClassComponent />
   </StrictMode>,
 )
